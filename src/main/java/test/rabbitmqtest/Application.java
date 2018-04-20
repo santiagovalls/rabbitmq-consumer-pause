42ebc6ee376d8648efa0f7536e493aa0ecd105cc
santiagovalls@gmail.com <santiagovalls@gmail.com>
2018-04-20 16:41:46 -0300

    Add SSLSocketFactoryWrapper & properties to test RabbitMQ Compose publishing

--- src/main/java/test/rabbitmqtest/Application.java
+++ src/main/java/test/rabbitmqtest/Application.java
@@ -1,61 +1,97 @@
 package test.rabbitmqtest;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.net.ssl.SNIHostName;
+import javax.net.ssl.SNIServerName;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
+import javax.net.ssl.SSLSocketFactory;
+import org.springframework.amqp.core.AcknowledgeMode;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.StringUtils;
 
 @SpringBootApplication
 public class Application {
 
-  final static String queueName = "spring-boot";
+  @Value("${spring.rabbitmq.concurrentConsumers}")
+  private Integer BROKER_CONCURRENT_CONSUMERS;
+
+  @Value("${spring.rabbitmq.maxConcurrentConsumers}")
+  private Integer BROKER_MAX_CONCURRENT_CONSUMERS;
+
+  @Value("${spring.rabbitmq.listener.retry.max-attempts}")
+  private Integer CONSUMER_MAX_RETRIES;
+
+  @Value("${spring.rabbitmq.addresses}")
+  private String addresses;
+
+  @Value("${spring.rabbitmq.host}")
+  private String rabbitHost;
 
   @Bean
-  Queue queue() {
-    return new Queue(queueName, false);
+  public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactoryWithTLS(
+      CachingConnectionFactory connectionFactory) {
+
+    if (StringUtils.isEmpty(addresses)) {
+      throw new IllegalArgumentException("Addresses must not be null or empty.");
+    }
+
+    connectionFactory.setUri(addresses);
+
+    SSLContext sslContext;
+
+    try {
+      sslContext = SSLContext.getInstance("TLSv1.2");
+      sslContext.init(null, null, null);
+
+      SSLContext.setDefault(sslContext);
+      SSLParameters sslParameters = sslContext.getDefaultSSLParameters();
+
+      List<SNIServerName> sniHostNames = new ArrayList<>(1);
+      sniHostNames.add(new SNIHostName(rabbitHost));
+      sslParameters.setServerNames(sniHostNames);
+
+      SSLSocketFactory wrappedSSLSocketFactory = new SSLSocketFactoryWrapper(
+          sslContext.getSocketFactory(), sslParameters);
+      connectionFactory.getRabbitConnectionFactory().setSocketFactory(wrappedSSLSocketFactory);
+
+    } catch (Exception e) {
+      throw new RuntimeException(e.getMessage());
+    }
+
+    SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+    factory.setConnectionFactory(connectionFactory);
+    factory.setConcurrentConsumers(BROKER_CONCURRENT_CONSUMERS);
+    factory.setMaxConcurrentConsumers(BROKER_MAX_CONCURRENT_CONSUMERS);
+    factory.setAcknowledgeMode(AcknowledgeMode.AUTO);
+    factory.setAutoStartup(true);
+    return factory;
   }
 
-  @Bean
-  TopicExchange exchange() {
-    return new TopicExchange("spring-boot-exchange");
-  }
-
-  @Bean
-  Binding binding(Queue queue, TopicExchange exchange) {
-    return BindingBuilder.bind(queue).to(exchange).with(queueName);
-  }
-
-  @Bean
-  @Qualifier("containerPause")
-  SimpleMessageListenerContainer containerPause(ConnectionFactory connectionFactory) {
-    SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-    container.setConnectionFactory(connectionFactory);
-    container.setQueueNames(queueName);
-    container.setMessageListener(
-        new MessageListenerAdapter(new Receiver("containerPause"), "receiveMessage"));
-    container.setConcurrentConsumers(5);
-    container.setMaxConcurrentConsumers(5);
-    return container;
-  }
-
-  @Bean
-  @Qualifier("container")
-  SimpleMessageListenerContainer container(ConnectionFactory connectionFactory) {
-    SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-    container.setConnectionFactory(connectionFactory);
-    container.setQueueNames(queueName);
-    container.setMessageListener(
-        new MessageListenerAdapter(new Receiver("container"), "receiveMessage"));
-    container.setConcurrentConsumers(5);
-    container.setMaxConcurrentConsumers(5);
-    return container;
+  // @Bean
+  public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+      CachingConnectionFactory connectionFactory) {
+    SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+    factory.setConnectionFactory(connectionFactory);
+    factory.setConcurrentConsumers(BROKER_CONCURRENT_CONSUMERS);
+    factory.setMaxConcurrentConsumers(BROKER_MAX_CONCURRENT_CONSUMERS);
+    factory.setAcknowledgeMode(AcknowledgeMode.AUTO);
+    factory.setAutoStartup(true);
+    return factory;
   }
 
   public static void main(String[] args) throws InterruptedException {
--- src/main/java/test/rabbitmqtest/StartStopController.java
+++ src/main/java/test/rabbitmqtest/StartStopController.java
@@ -5,43 +5,17 @@ import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StartStopController {
 
-  public static boolean RECEIVER_THROW_EXCEPTION_FLAG = false;
-
-  @Autowired
-  @Qualifier("containerPause")
-  SimpleMessageListenerContainer container;
-
   @Autowired
   RabbitTemplate rabbitTemplate;
 
-  @RequestMapping("/startConsumer")
-  public String startConsumer() {
-    container.start();
-    return "startConsumer";
-  }
-
-  @RequestMapping("/stopConsumer")
-  public String stopConsumer() {
-    container.stop();
-    return "stopConsumer";
-  }
-
   @RequestMapping("/publishTestMessage")
   public String publishTestMessage() {
-    rabbitTemplate.convertAndSend(Application.queueName, "Test Message");
+    rabbitTemplate.convertAndSend("rabbitmq-compose-test", "Test Message");
     return "publishTestMessage";
-  }
-
-  @RequestMapping("/configThrowException")
-  public String throwException(@RequestParam boolean value) {
-    StartStopController.RECEIVER_THROW_EXCEPTION_FLAG = value;
-    return "configThrowException: " + Boolean
-        .toString(StartStopController.RECEIVER_THROW_EXCEPTION_FLAG);
   }
 }
