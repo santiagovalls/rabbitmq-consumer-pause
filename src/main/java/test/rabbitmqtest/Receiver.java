c7b60984bfdfc4e74e86154ab9d66ef269ba0ca1
santiagovalls@gmail.com <santiagovalls@gmail.com>
2018-04-25 14:25:55 -0300

    Add priority to messages for test purposes

--- src/main/java/test/rabbitmqtest/Receiver.java
+++ src/main/java/test/rabbitmqtest/Receiver.java
@@ -1,28 +1,16 @@
 package test.rabbitmqtest;
 
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Receiver {
 
-  private String name;
-
-  public Receiver(String name) {
-    this.name = name;
-  }
-
-  public void receiveMessage(String message) {
-    System.out.println("Receiver <" + name + "> - Received <" + message + ">");
-    sleepTenSeconds();
-    if (!StartStopController.RECEIVER_THROW_EXCEPTION_FLAG) {
-      System.out.println("Receiver <" + name + "> - Finish!");
-    } else {
-      throw new RuntimeException("POC Receiver Exception!");
-    }
-  }
-
-  private void sleepTenSeconds() {
-    try {
-      Thread.sleep(10000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
+  @RabbitListener(queues = "rabbitmq-compose-test")
+  public void onMessageReceived(Message message) {
+    System.out.println(
+        "Received <" + message.getBody() + "> - Priority <" + message.getMessageProperties()
+            .getPriority() + ">");
   }
 }
--- src/main/java/test/rabbitmqtest/StartStopController.java
+++ src/main/java/test/rabbitmqtest/StartStopController.java
@@ -1,47 +1,29 @@
 package test.rabbitmqtest;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StartStopController {
 
-  public static boolean RECEIVER_THROW_EXCEPTION_FLAG = false;
-
-  @Autowired
-  @Qualifier("containerPause")
-  SimpleMessageListenerContainer container;
-
   @Autowired
   RabbitTemplate rabbitTemplate;
 
-  @RequestMapping("/startConsumer")
-  public String startConsumer() {
-    container.start();
-    return "startConsumer";
-  }
-
-  @RequestMapping("/stopConsumer")
-  public String stopConsumer() {
-    container.stop();
-    return "stopConsumer";
-  }
-
-  @RequestMapping("/publishTestMessage")
-  public String publishTestMessage() {
-    rabbitTemplate.convertAndSend(Application.queueName, "Test Message");
-    return "publishTestMessage";
-  }
-
-  @RequestMapping("/configThrowException")
-  public String throwException(@RequestParam boolean value) {
-    StartStopController.RECEIVER_THROW_EXCEPTION_FLAG = value;
-    return "configThrowException: " + Boolean
-        .toString(StartStopController.RECEIVER_THROW_EXCEPTION_FLAG);
+  @RequestMapping("/publishTestMessages")
+  public String publishTestMessages() {
+    for (int i = 0; i < 10; i++) {
+      final int priority = i;
+      rabbitTemplate.convertAndSend(
+          "rabbitmq-compose-test",
+          "Test Message: " + priority,
+          message -> {
+            message.getMessageProperties().setPriority(priority);
+            return message;
+          }
+      );
+    }
+    return "publishTestMessages";
   }
 }
